94f38710d1b775eedd7565bb587bc6feb8858ff8
Jennifer Emanuele <jemanuele@gmail.com>
2022-01-15 13:43:45 -0500

    all tests passing, x 11

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -4,7 +4,7 @@ const Intern = require('../lib/Intern');
 test('creates Intern ', () => {
     const intern = new Intern('Jen', 25, 'jemanuele@gmail.com', 'OSU');
 
-    expect(intern.school.toEqual(expect.any(String)));
+    expect(intern.school).toEqual(expect.any(String));
 });
 
 test('gets role of employee', () => {
