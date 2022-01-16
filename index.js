05c26d8ef092c08f073a3e25aaa3fb2ebacae800
Jennifer Emanuele <jemanuele@gmail.com>
2022-01-16 17:36:14 -0500

    fixed gen page issues, css added

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ const { prompt } = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
+
+
+const generatePage = require('./src/generatePage');
+
+const { stringify } = require('querystring');
 const  teamRoster = {'Manager':[],'Engineer':[],'Intern':[]};
 var position = '';
 
 
-//?????????????????
-prompt({
+prompt([
+    {
     type: 'list',
     name: 'role',
     message: 'Which role would you like to pick?',
     choices: Object.keys(teamRoster)
-}).then(({role}) => {
+}
+])
+.then(({role}) => {
+    // build the questions list
     position = role;
     let items = ['name','id','email'];
     items.push(role == 'Manager' ? 'officeNumber' : role == 'Engineer' ? 'Github' : 'school');
@@ -21,22 +30,34 @@ prompt({
     items.forEach(item => {
         questions.push({type:'input',name:item,message:`What is your ${item}?`})
     });
+   
+;
+    // now take the list we built, and prompt the user for each one.
     prompt(questions).then(answers => {
-               
+
         if (role === 'Manager') {
-            const manager = new Manager(answers.name, answers.id, answers.email);
-            teamRoster.Manager.push(manager)
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            teamRoster.Manager.push(manager);            
         } 
         if (role === 'Engineer') {
-            const engineer = new Engineer(answers.name, answers.id, answers.email);
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
             teamRoster.Engineer.push(engineer)
         }
         if (role === 'Intern') {
-            const intern = new Intern(answers.name, answers.id, answers.email);
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
             teamRoster.Intern.push(intern)
         }
-       console.log(teamRoster);
+
+      // pass in the entire team roster
+        var stringHtml = generatePage(teamRoster);
+        // console.log(stringHtml);
+        writeHtmlFile(stringHtml);               
+        
     });
+
+    // once the team roster is built, then send it to the file
+    // generateTeamHtml(generatePage.generateManager(teamRoster.Manager[0])); 
+    //generateTeamHtml(teamRosster)); 
    
     
 });
@@ -44,8 +65,9 @@ prompt({
 
 // create function to write html file **const writeHtml = data =>
 
-const writeHtml = teamRoster => {
-    fs.writeFile('./dist/index.html', teamRoster, err => {
+const writeHtmlFile = stringHtml => {
+
+    fs.writeFile('./dist/index.html', stringHtml, err => {
         if (err) {
             console.log(err);
             return;
@@ -54,6 +76,6 @@ const writeHtml = teamRoster => {
         }
     })
 
-writeHtml();
+
 };
 
---
+++ src/generatePage.js
@@ -0,0 +1,136 @@
+const Employee = require("../lib/Employee");
+const Manager = require("../lib/Manager")
+
+
+ // make Manager card
+ const generateManager = function (manager) {
+    return `
+    <div  class="col-4 mt-3">
+        <div class="card h-85">
+            <div class="card-header">
+                <h3>${manager.name}: Manager</h3>
+            </div>
+
+            <div class="card-body">
+                <p class="id"> ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class= "officeNumber">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>    
+    `
+}
+
+ // make Engineer card
+ const generateEngineer = function (engineer) {
+    return `
+    <div  class="col-4 mt-3">
+        <div class="card h-85">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+            </div>
+
+            <div class="card-body">
+                <p class="id"> ID: ${engineer.id}: Engineer</p>
+                <p class="email">Email: <a href="mailto:${engineer.emaill}">${engineer.email}</a></p>
+                <p class= "github">Github: <a href="https://github.com/${engineer.github}"></a></p>
+            </div>
+        </div>
+    </div>    
+`
+ }
+
+ // make  Intern card
+ const generateIntern = function (intern) {
+    return `
+    <div  class="col-4 mt-3"
+        <div class="card">
+            <div class="card-header">
+                <h3>${intern.name}: Intern</h3>
+            </div>
+
+            <div class="card-body">
+                <p class="id"> ID: ${intern.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.emaill}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>    
+    `
+ };
+
+ // place array to page
+ generatePage = (teamRoster) => {
+
+    // console.log("********************************" +JSON.stringify(   teamRoster ));
+    // console.log("********************************" +JSON.stringify(   teamRoster.Manager.length ));
+    var teamRoster2 = [];
+    for (let i =0; i < teamRoster.Manager.length; i++) {
+        const employee = teamRoster.Manager[i];
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+
+            teamRoster2.push(managerCard)
+        }
+
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+
+            teamRoster2.push(engineerCard);
+        }
+
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+
+            teamRoster2.push(internCard);
+        }
+        // joining
+        const teamCards = teamRoster2.join('')
+
+        // place on generated page
+        const generateTeam = generateTeamHtml(teamCards);
+        return generateTeam;
+    }
+
+
+ }
+// generateTeamHtml using teamRoster array
+const generateTeamHtml = function (teamCards) {
+
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Team Roster</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+        
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <h1 span class="navbar-brand text-center" id="navbar-text">My Team<h1>            
+        </header> 
+        <main>
+            <div class="container">
+                <div class= "row justify-center" id="team-cards">
+                    ${teamCards}
+                </div>
+            </div>
+        </main>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </html>
+`
+}
+
+
+// export to index page
+module.exports = generatePage;
